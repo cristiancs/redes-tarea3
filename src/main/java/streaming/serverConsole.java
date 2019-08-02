c30b83c46f489cde65d0983886b2367a83ea9f54
CristiaN <cristiannavarrete@outlook.com>
2019-08-02 11:38:26 -0400

    LS del servidor

--- src/main/java/streaming/serverConsole.java
+++ src/main/java/streaming/serverConsole.java
@@ -1,14 +1,41 @@
 package streaming;
 
+import java.io.File;
+
 public class serverConsole implements Runnable {
     @Override
     public void run() {
         String inConsole = "";
-        System.out.println(
-                "Seleccione un comando \n1) Mostrar videos disponibles \n2) Reproducir Video\n3) Apagar el Servidor");
+
         while (true) {
+            System.out.println(
+                    "Seleccione un comando \n1) Mostrar videos disponibles \n2) Reproducir Video\n3) Apagar el Servidor");
             inConsole = System.console().readLine();
-            System.out.println(inConsole);
+            if (inConsole.equals("1")) {
+                File curDir = new File("./media");
+                File[] filesList = curDir.listFiles();
+                for (File f : filesList) {
+                    System.out.println(f.getName());
+                }
+            }
+            if (inConsole.equals("2")) {
+                System.out.println("Ingrese Nombre video");
+                inConsole = System.console().readLine();
+                File tempFile = new File("./media/" + inConsole);
+                boolean exists = tempFile.exists();
+                if (exists) {
+                    // To Do start player
+                } else {
+                    System.out.println("Video no disponible");
+                }
+
+            }
+            if (inConsole.equals("3")) {
+                // To Do: should close connections
+            } else {
+                System.out.println("Comando no reconocido");
+            }
+
         }
     }
 
