9b29b49b712a7cf03dc9e8c78af65d44d784ecaf
CristiaN <cristiannavarrete@outlook.com>
2019-08-02 22:13:51 -0400

    Agregadas funciones para permitir concurrencia y listen to observer.

--- HELPERS/VideoHandler.java
+++ HELPERS/VideoHandler.java
@@ -8,48 +8,63 @@ import javax.imageio.ImageIO;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameConverter;
+import java.util.concurrent.TimeUnit;
 
 import streaming.StreamObservable;
 import streaming.Utils;
 
-public class VideoHandler {
+public class VideoHandler implements Observer, Runnable {
     private StreamObservable observable;
+    private Boolean continuePlaying = false;
 
-    public void start(String archivo) {
-        try {
-            FFmpegFrameGrabber g = new FFmpegFrameGrabber(archivo);
-            Java2DFrameConverter converter = new Java2DFrameConverter();
-            Utils utils = new Utils();
-            g.start();
-            Frame frame = null;
-            while ((frame = g.grab()) != null) {
-                if (frame.image != null) {
-                    BufferedImage buf = converter.convert(frame);
-                    Integer server = 0;
-                    if (buf != null) {
-                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                        ImageIO.write(buf, "jpg", baos);
-                        baos.flush();
-                        byte[] imageInByte = baos.toByteArray();
+    @Override
+    public void update(Observable o, Object arg) {
+        String mensaje = arg.toString();
 
-                        observable.sendStream(utils.encodeBytesToBase64String(imageInByte), server);
-                        // System.out.println(utils.encodeBytesToBase64String(imageInByte));
-                        baos.close();
-                        server = (server + 1) % 2;
-
-                    }
-                }
-            }
-
-            g.stop();
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
+        if (arg.toString().equals("stop")) {
+            continuePlaying = false;
+        } else if (arg.toString().startsWith("streaming")) {
+            continuePlaying = true;
         }
+
     }
 
-    public static void main(String[] args) {
-        App app = new App();
-        app.start();
+    @Override
+    public void start() {
+        while (true) {
+            if (continuePlaying) {
+                try {
+                    FFmpegFrameGrabber g = new FFmpegFrameGrabber(archivo);
+                    Java2DFrameConverter converter = new Java2DFrameConverter();
+                    Utils utils = new Utils();
+                    g.start();
+                    Frame frame = null;
+                    while ((frame = g.grab()) != null && continuePlaying) {
+                        if (frame.image != null) {
+                            BufferedImage buf = converter.convert(frame);
+                            Integer server = 0;
+                            if (buf != null) {
+                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                ImageIO.write(buf, "jpg", baos);
+                                baos.flush();
+                                byte[] imageInByte = baos.toByteArray();
+
+                                observable.sendStream(utils.encodeBytesToBase64String(imageInByte), server);
+                                // System.out.println(utils.encodeBytesToBase64String(imageInByte));
+                                baos.close();
+                                server = (server + 1) % 2;
+
+                            }
+                        }
+                    }
+
+                    g.stop();
+                } catch (Exception e) {
+                    System.out.println("Error: " + e);
+                }
+            }
+            TimeUnit.MILLISECONDS.sleep(40);
+        }
 
     }
 
